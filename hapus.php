1a9ab83b19020e489a36a084cb7d0530253fb4a3
miftahuljannah4 <81582495+miftahuljannah4@users.noreply.github.com>
2021-06-20 00:57:41 +0700

    Add files via upload

--- hapus.php
+++ hapus.php
@@ -2,7 +2,11 @@
 <?php
     include 'config.php';
     $id = $_GET['id'];
-    $sql = "DELETE FROM pembeli WHERE id_pmb = '{$id}'";
-    $result = mysqli_query($conn, $sql);
-    //header('location: index.php');
+
+    $result = mysqli_query($conn, "DELETE FROM produk WHERE id_prd = '{$id}'");
+    $result = mysqli_query($conn, "DELETE FROM pembeli WHERE id_pmb = '{$id}'");
+    $result = mysqli_query($conn, "DELETE FROM transaksi WHERE id_trs = '{$id}'");
+    $result = mysqli_query($conn, "DELETE FROM pembayaran WHERE id_bayar = '{$id}'");
+
+    header('location: index.php');
 ?>
